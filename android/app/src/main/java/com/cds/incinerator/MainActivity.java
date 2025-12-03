4dc927859b094dfdcf7548e8d376bbc1640f1713
mahaneim-temp <mahaneim@gmail.com>
2025-12-04 00:59:18 +0900

    Fix MainActivity for landscape mode and WebView settings

--- android/app/src/main/java/com/cds/incinerator/MainActivity.java
+++ android/app/src/main/java/com/cds/incinerator/MainActivity.java
@@ -1,6 +1,7 @@
 package com.cds.incinerator;
 
 import android.annotation.SuppressLint;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebSettings;
@@ -16,6 +17,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        // 화면 방향을 가로 모드로 강제 설정 (가장 먼저 실행)
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        
         setContentView(R.layout.activity_main);
 
         webView = findViewById(R.id.webview);
@@ -26,16 +31,15 @@ public class MainActivity extends AppCompatActivity {
         webSettings.setDomStorageEnabled(true);
         webSettings.setAllowFileAccess(true);
         webSettings.setAllowContentAccess(true);
-        webSettings.setLoadWithOverviewMode(true);
         webSettings.setUseWideViewPort(true);
+        webSettings.setLoadWithOverviewMode(false);
         webSettings.setBuiltInZoomControls(true);
         webSettings.setDisplayZoomControls(false);
         webSettings.setSupportZoom(true);
         
-        // 줌 제스처 활성화
-        webSettings.setSupportZoom(true);
-        webSettings.setBuiltInZoomControls(true);
-        webSettings.setDisplayZoomControls(false);
+        // 모바일 최적화 - 스케일링 비활성화하여 CSS transform이 제대로 작동하도록
+        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NORMAL);
+        webView.setInitialScale(100);
         
         // 캐시 설정
         webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
@@ -50,6 +54,13 @@ public class MainActivity extends AppCompatActivity {
         
         // 로컬 HTML 파일 로드
         webView.loadUrl("file:///android_asset/index.html");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // 화면이 다시 활성화될 때도 가로 모드 유지
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
 
     @Override
@@ -69,4 +80,3 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
     }
 }
-
